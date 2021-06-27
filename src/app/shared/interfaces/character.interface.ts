ac1a6ca812252d270a042bd53ad15279590394a8
Henrique Petrelli <henriquepetrelli96@gmail.com>
2021-06-27 16:03:01 -0300

    Auto-generated commit [ci skip]

--- src/app/shared/interfaces/character.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface Character {
-  id: number;
-  name: string;
-  image: string;
-  species: string;
-  gender: string;
-  created: string;
-  status: string;
-}
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  baseUrl: 'https://rickandmortyapi.com/api/',
-  production: false
-};
