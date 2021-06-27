21ce1fd25434d7306cc8c1f96e7b28e1a6974c85
Henrique Petrelli <henriquepetrelli96@gmail.com>
2021-06-27 15:22:08 -0300

    fix(deploy): interestCohort

--- src/app/shared/components/home/home.component.ts
+++ src/app/shared/components/home/home.component.ts
@@ -10,5 +10,9 @@ export class HomeComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit(): void {}
+  async ngOnInit(): Promise<void> {
+    const { id, version } = await document.interestCohort();
+  }
+
+  
 }
